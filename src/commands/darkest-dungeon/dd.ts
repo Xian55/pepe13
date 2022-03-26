801506415391e040a9d15233f54d06f579802728
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-26 19:43:24 +0100

    Added darkest dungeon plugin which plays local file to voice channel!

---
+++ src/commands/darkest-dungeon/dd.ts
@@ -0,0 +1,133 @@
+import { Command } from "../../structures/Command";
+import { client } from "../..";
+import { Track, RawTrackData } from "discord-player";
+import path from "path";
+import fs from "fs";
+
+const { player } = client;
+const dataPath = path.resolve(__dirname, "../../../data/darkest/");
+
+export default new Command({
+    name: 'dd',
+    description: 'Return with a Darkest Dungeon quote',
+    options: [
+        {
+            name: 'query',
+            type: 'STRING' as const,
+            description: '[search term]',
+            required: false,
+        },
+    ],
+    run: async ({ interaction }) => {
+
+        const { options, member, guild, guildId, channel } = interaction;
+        const query = options.getString("query");
+        const { key, description } = findBestQuote(query);
+
+        const queue = player.createQueue(guild, {
+            metadata: channel,
+            autoSelfDeaf: false,
+            bufferingTimeout: 100,
+        });
+
+        try {
+            if (!queue.connection) await queue.connect(member.voice.channel);
+        } catch {
+            void player.deleteQueue(guildId);
+            return void interaction.followUp({ content: "Could not join your voice channel!" });
+        }
+
+        await interaction.followUp({ content: `⏱ | Playing quote '${description}'` });
+
+        const filePath = path.join(dataPath, "voice-data", key);
+        if (!fs.existsSync(filePath)) return;
+
+        const raw = {
+            engine: () => fs.createReadStream(filePath)
+        }
+
+        const data: RawTrackData = {
+            title: description,
+            description: "1900",
+            author: "Darkest Dungeon",
+            url: filePath,
+            thumbnail: "string",
+            duration: "10s",
+            views: 0,
+            requestedBy: member.user,
+            source: "arbitrary",
+            raw: raw
+        };
+
+        let track = new Track(player, data);
+        queue.addTrack(track);
+        if (!queue.playing) await queue.play();
+    }
+})
+
+function findBestQuote(suffix: string) {
+    let db = require(path.join(dataPath, "map.json"));
+    let highestScore = 0;
+    let bestKey: string;
+
+    if (!suffix || suffix.length == 0) {
+        bestKey = randomKey(db);
+    } else {
+        for (var key in db) {
+            let value = similarity(db[key], suffix);
+
+            if (value > highestScore) {
+                highestScore = value;
+                bestKey = key;
+            }
+        }
+    }
+
+    return { key: bestKey, description: db[bestKey] };
+}
+
+const randomKey = function (obj) {
+    var keys = Object.keys(obj);
+    return keys[keys.length * Math.random() << 0];
+};
+
+function similarity(s1, s2) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+        longer = s2;
+        shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+        return 1.0;
+    }
+    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+}
+
+function editDistance(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    var costs = new Array();
+    for (var i = 0; i <= s1.length; i++) {
+        var lastValue = i;
+        for (var j = 0; j <= s2.length; j++) {
+            if (i == 0)
+                costs[j] = j;
+            else {
+                if (j > 0) {
+                    var newValue = costs[j - 1];
+                    if (s1.charAt(i - 1) != s2.charAt(j - 1))
+                        newValue = Math.min(Math.min(newValue, lastValue),
+                            costs[j]) + 1;
+                    costs[j - 1] = lastValue;
+                    lastValue = newValue;
+                }
+            }
+        }
+        if (i > 0)
+            costs[s2.length] = lastValue;
+    }
+    return costs[s2.length];
+}
