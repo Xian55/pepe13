2fe73ffa1b68c969888ddb65aa5095a7a387d350
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-26 20:22:01 +0100

    Added sailor plugin with timed cron job to submit a video.

--- environment.d.ts
+++ environment.d.ts
@@ -6,6 +6,7 @@ declare global {
             guildId: string;
             MONGO_URI: string;
             environment: "dev" | "prod" | "debug";
+            plebek_post: string;
         }
     }
 }
---
+++ src/events/ready_sailor.ts
@@ -0,0 +1,34 @@
+import { Event } from "../structures/Events";
+import { logHandler } from "../utils/logHandler";
+import { CronJob } from 'cron';
+import path from "path";
+import fs from "fs";
+import { client } from "..";
+import { TextChannel } from "discord.js";
+
+export default new Event('ready', async () => {
+    if (!fs.existsSync(mondayFile)) return;
+    let job = new CronJob('0 4 * * MON', task);
+    job.start();
+});
+
+const mondayFile = path.resolve(__dirname, "../../user-data/it_be_monday.mp4")
+
+async function task() {
+    const channel = await client.channels.fetch(process.env.plebek_post); // playground - 789904472863146047
+    if (!channel) return;
+    const textChannel = channel as TextChannel;
+
+    textChannel.send(
+        {
+            files: [
+                {
+                    attachment: mondayFile,
+                    name: `its_monday.mp4`,
+                    description: "its_monday"
+                }
+            ]
+        }
+    );
+
+}
