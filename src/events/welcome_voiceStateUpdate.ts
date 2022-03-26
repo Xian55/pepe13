6301353108943ede03ed1784997a2ec122111bce
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-26 18:00:54 +0100

    Added welcome plugin with music playback.

---
+++ src/events/welcome_voiceStateUpdate.ts
@@ -0,0 +1,52 @@
+import { VoiceState } from "discord.js";
+import { client } from "..";
+import { Event } from "../structures/Events";
+import { logHandler } from "../utils/logHandler";
+
+import { QueryType } from "discord-player";
+import Schema from "../schemas/welcome";
+
+const { player } = client;
+
+export default new Event('voiceStateUpdate',
+    async (oldState: VoiceState, newState: VoiceState) => {
+        if (newState.id == client.user.id ||
+            !newState.channelId ||
+            oldState.channelId == newState.channelId) return;
+
+        const { guild, id, channel } = newState
+
+        const data = await Schema.findOne(
+            { Guild: guild.id, Member: id }
+        );
+        if (!data) return;
+
+        const { Link } = data;
+
+        const searchResult = await player
+            .search(Link, {
+                requestedBy: newState.member.user,
+                searchEngine: QueryType.YOUTUBE_VIDEO
+            })
+            .catch(console.warn);
+
+        if (!searchResult || !searchResult.tracks.length) return;
+
+        const queue = player.createQueue(guild, {
+            metadata: channel,
+            autoSelfDeaf: false,
+            bufferingTimeout: 500
+        });
+
+        try {
+            if (!queue.connection)
+                await queue.connect(channel);
+        } catch (e) {
+            void player.deleteQueue(guild.id);
+            console.log(e);
+        }
+
+        logHandler.log("debug", `⏱ | Loading ${Link} as welcome ...`)
+        if (!queue.playing)
+            await queue.play(searchResult.tracks[0]);
+    })
--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -1,10 +1,11 @@
-import { ApplicationCommandDataResolvable, Client, ClientEvents, Collection, TextBasedChannel } from "discord.js";
+import { AnyChannel, ApplicationCommandDataResolvable, Client, ClientEvents, Collection, TextBasedChannel } from "discord.js";
 import { CommandType } from "../typings/Commands";
 import glob from "glob";
 import { promisify } from "util";
 import { RegisterCommandsOptions } from "../typings/client";
 import { Event } from "./Events";
 import { logHandler } from "../utils/logHandler";
+import { errorHandler } from "../utils/errorHandler";
 import { ChatFilter } from "../typings/ChatFilter";
 import initChatFilter from "../systems/chatfilter/chatFilter"
 import { Player } from "discord-player";
@@ -19,41 +20,66 @@ export class ExtendedClient extends Client {
     constructor() {
         super({ intents: 32767 })
 
-        this.player = new Player(this);
+        this.player = new Player(this, {
+            ytdlOptions: {
+                begin: 0,
+                filter: 'audioonly'
+            }
+        });
         this.player.on("error", (queue, error) => {
-            console.log(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
+            errorHandler(`[${queue.guild.name}] Error emitted from the queue: ${error.message}`, error);
         });
         this.player.on("connectionError", (queue, error) => {
-            console.log(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`);
+            errorHandler(`[${queue.guild.name}] Error emitted from the connection: ${error.message}`, error);
         });
 
+        if (process.env.environment == "debug" || process.env.environment == "dev") {
+            this.player.on("debug", (queue, message) => {
+                logHandler.log("debug", `[${queue.guild.name}] ${message}`);
+            });
+        }
+
         this.player.on("trackStart", (queue, track) => {
+            const channel = queue.metadata as AnyChannel;
+            if (channel.isVoice()) {
+                logHandler.log("debug", `🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
+                return;
+            }
             const textChannel = queue.metadata as TextBasedChannel;
-            if (!textChannel) return;
             textChannel.send(`🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`);
         });
 
         this.player.on("trackAdd", (queue, track) => {
+            const channel = queue.metadata as AnyChannel;
+            if (channel.isVoice()) {
+                console.log(`🎶 | Track **${track.title}** queued!`);
+                return;
+            }
             const textChannel = queue.metadata as TextBasedChannel;
-            if (!textChannel) return;
             textChannel.send(`🎶 | Track **${track.title}** queued!`);
         });
 
         this.player.on("botDisconnect", (queue) => {
+            const channel = queue.metadata as AnyChannel;
+            if (channel.isVoice()) {
+                console.log("❌ | I was manually disconnected from the voice channel, clearing queue!");
+                return;
+            }
             const textChannel = queue.metadata as TextBasedChannel;
-            if (!textChannel) return;
             textChannel.send("❌ | I was manually disconnected from the voice channel, clearing queue!");
         });
 
         this.player.on("channelEmpty", (queue) => {
+            const channel = queue.metadata as AnyChannel;
+            if (channel.isVoice()) return;
             const textChannel = queue.metadata as TextBasedChannel;
-            if (!textChannel) return;
             textChannel.send("❌ | Nobody is in the voice channel, leaving...");
         });
 
         this.player.on("queueEnd", (queue) => {
+            const channel = queue.metadata as AnyChannel;
+            if (channel.isVoice()) return;
             const textChannel = queue.metadata as TextBasedChannel;
-            if (!textChannel) return;
             textChannel.send("✅ | Queue finished!");
         });
     }
