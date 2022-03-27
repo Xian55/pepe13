e7f3ee09063b44ccac4d3f71fefe16afcc399d51
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-27 16:03:12 +0200

    youtube timed: ignore bot messages

--- src/systems/chatfilter/filters/yt-timed.ts
+++ src/systems/chatfilter/filters/yt-timed.ts
@@ -1,5 +1,6 @@
 export default {
     async run({ message }) {
+        if (message.author.bot) return;
         const { content } = message;
         let regex = /https:\/\/(www\.|)youtu(be\.com|\.be)\/[\S]*(\?|&)t=[\d]+(\n| |)/im;
         if (!regex.test(content)) return;
