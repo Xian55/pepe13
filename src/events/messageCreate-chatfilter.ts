05917efbdaf2906a8cb94929b5e31d8758bd513d
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-27 15:59:50 +0200

    rename messageCreate with chatfilter

--- src/events/messageCreate-chatfilter.ts
+++ src/events/messageCreate-chatfilter.ts
@@ -1,19 +1,13 @@
 import { client } from "..";
 import { Event } from "../structures/Events";
 
-export default new Event('messageCreate', async (message) => {
-    if (message.author.bot) return;
-
+export default [new Event('messageCreate', async (message) => {
     const { guild, channel } = message;
-
     const channelHasFilter = client.chatFilter.channels.get(guild.id);
     if (!channelHasFilter) return;
 
     let handlers = client.chatFilter.handlers.get(channel.id);
-
-    //console.log(handlers);
-
     handlers.forEach((handler) => {
         client.chatFilter.filters.get(handler)?.run({ client, message });
     });
-})
+})]
