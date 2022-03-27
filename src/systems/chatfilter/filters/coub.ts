c4cb5a619611693ef4c39245c782479839aca73e
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-27 16:02:21 +0200

    Coub constness and types

--- src/systems/chatfilter/filters/coub.ts
+++ src/systems/chatfilter/filters/coub.ts
@@ -4,20 +4,20 @@ import util from 'util';
 
 export default {
     async run({ message }) {
-        const { content } = message;
-        let regex = /https:\/\/(www\.|)coub\.com\/(view|embed)\//im;
+        const { content, channel } = message;
+        const regex = /https:\/\/(www\.|)coub\.com\/(view|embed)\//im;
         if (!regex.test(content)) return;
 
-        let safeUrl = content.replace(/[||]/g, '');
-        let spoiler = content != safeUrl;
+        const safeUrl = content.replace(/[||]/g, '');
+        const spoiler = content != safeUrl;
 
         console.log(`processing ${safeUrl}`);
 
-        let serverPath = `${__dirname}./../../../../tmp/`;
-        let id = safeUrl.split('/').pop();
-        let savePath = path.join(serverPath, (spoiler ? 'SPOILER_' : '') + id + '.mp4');
+        const serverPath = `${__dirname}./../../../../tmp/`;
+        const id = safeUrl.split('/').pop();
+        const savePath = path.join(serverPath, (spoiler ? 'SPOILER_' : '') + id + '.mp4');
 
-        let cmd = `coub-dl -i ${safeUrl} -o ${savePath} --format mp4 --scale 400 --loop 10 --time 12`; // -C
+        const cmd = `coub-dl -i ${safeUrl} -o ${savePath} --format mp4 --scale 400 --loop 10 --time 12`; // -C
         await execAsync(cmd);
 
         if (!fs.existsSync(savePath)) {
@@ -25,7 +25,7 @@ export default {
             return;
         }
 
-        await message.channel.send(
+        await channel.send(
             {
                 files: [
                     {
@@ -38,10 +38,6 @@ export default {
                 await message.delete({ timeout: 1000 })
                     .then(() => console.log(`Deleted request of ${content}`))
                     .catch(console.error);
-
-                //if (memeCoin != null)
-                //    await rsp.react(memeCoin.id);
-
             }).catch(console.log);
 
         deleteFile(savePath);
@@ -49,7 +45,7 @@ export default {
 }
 
 
-async function execAsync(cmd) {
+async function execAsync(cmd: string) {
     try {
         const exec = util.promisify(require('child_process').exec);
 
@@ -67,7 +63,7 @@ async function execAsync(cmd) {
     }
 }
 
-function deleteFile(path) {
+function deleteFile(path: string) {
     if (fs.existsSync(path)) {
         try {
             fs.unlinkSync(path);
