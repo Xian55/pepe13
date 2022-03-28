7cba4405b841faf3fd7ef73d2caa73d4177c54f0
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-28 15:20:59 +0200

    added command to end warcraftlog

---
+++ src/commands/warcraftlog/endlog.ts
@@ -0,0 +1,73 @@
+import { Message, MessageEmbed } from "discord.js";
+import { Command } from "../../structures/Command";
+
+const ironforgeBaseURL = "https://ironforge.pro/analyzer/report/"
+const regexLog = /https:\/\/classic\.warcraftlogs\.com\/reports\/(.*)\//;
+
+export default new Command({
+    name: "endlog",
+    description: "Ends the live warcraftlog! Inserts ironforge link.",
+    options: [
+        {
+            name: "message_id",
+            description: `Message id with contains the warcraft log bot message.`,
+            type: "STRING",
+            required: true
+        },
+        {
+            name: "title",
+            description: `The title of the the event`,
+            type: "STRING",
+            required: false
+        }
+    ],
+    run: async ({ interaction, args }) => {
+        const { channel } = interaction;
+
+        const title = args.getString("title") || "warcraftlog";
+
+        const messageId = args.getString("message_id");
+        const message = await channel.messages.fetch(messageId);
+        if (!message) return;
+
+        await interaction
+            .followUp({ content: 'ironforge link added to the log!', ephemeral: true })
+            .then((reply) => {
+                setTimeout(() => (reply as Message).delete(), 1000);
+            });
+
+        const { embeds } = message;
+
+        if (embeds.length == 1) {
+            const { url, description } = embeds[0];
+            if (regexLog.test(url)) {
+                const match = url.match(regexLog);
+                const id = match[1];
+
+                if (!id) return;
+
+                embeds[0].thumbnail = null;
+
+                if (embeds[0].title.length == 0) {
+                    if (description && description.length == 0) {
+                        embeds[0].title = title;
+                    }
+                    else {
+                        embeds[0].title = description;
+                    }
+                }
+
+                const ironforgeURL = ironforgeBaseURL + id + "/";
+                const forgeEmbed = new MessageEmbed()
+                    .setTitle("consumable usage")
+                    .setURL(ironforgeURL);
+
+                embeds.push(forgeEmbed);
+
+                await interaction.followUp({ embeds: embeds }).then(() => {
+                    setTimeout(() => (message as Message).delete(), 1000);
+                });
+            }
+        }
+    }
+})
