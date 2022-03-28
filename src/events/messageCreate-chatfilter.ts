9ecc1a32f8cf9eb147daef8e87b23f53cdb434ac
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-28 10:25:48 +0200

    messageCreate event deconstruct

--- src/events/messageCreate-chatfilter.ts
+++ src/events/messageCreate-chatfilter.ts
@@ -1,13 +1,17 @@
 import { client } from "..";
 import { Event } from "../structures/Events";
 
+const { chatFilter } = client;
+
 export default [new Event('messageCreate', async (message) => {
     const { guild, channel } = message;
-    const channelHasFilter = client.chatFilter.channels.get(guild.id);
+    const { channels, handlers, filters } = chatFilter;
+
+    const channelHasFilter = channels.get(guild.id);
     if (!channelHasFilter) return;
 
-    let handlers = client.chatFilter.handlers.get(channel.id);
-    handlers.forEach((handler) => {
-        client.chatFilter.filters.get(handler)?.run({ client, message });
+    const channelHandlers = handlers.get(channel.id);
+    channelHandlers.forEach((handler) => {
+        filters.get(handler)?.run({ client, message });
     });
 })]
