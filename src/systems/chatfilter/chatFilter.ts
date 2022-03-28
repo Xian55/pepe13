fa06a36c219ab2fca2989591640dbbce7e3aced5
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-28 10:52:57 +0200

    fix a possible bug when file extenstion shows up the the toggleable chat filter command

--- src/systems/chatfilter/chatFilter.ts
+++ src/systems/chatfilter/chatFilter.ts
@@ -9,18 +9,19 @@ import { IChatFilter } from "../../typings/ChatFilter";
 const globPromise = promisify(glob)
 
 export default async (client: ExtendedClient) => {
+    const { chatFilter } = client;
     const files = await globPromise(`${__dirname}/filters/*{.ts,.js}`);
-    files.forEach(async filePath => {
-        const basename = path.basename(filePath, '.ts');
-        const filter: IChatFilter = await client.importFile(filePath);
-        client.chatFilter.filters.set(basename, filter);
+    files.forEach(async file => {
+        const filter: IChatFilter = await client.importFile(file);
+        const fileName = path.parse(file).name;
+        chatFilter.filters.set(fileName, filter);
         if (process.env.environment == "debug")
-            logHandler.log("debug", `Added ${basename} -> ${filePath}`);
+            logHandler.log("debug", `Added ${fileName} -> ${file}`);
     });
 
     filter.find().then((documents) => {
         documents.forEach((doc) => {
-            client.chatFilter.populate(doc as FilterInt);
+            chatFilter.populate(doc as FilterInt);
             if (process.env.environment == "debug")
                 logHandler.log("debug", `ChatFilter [${doc.Handler}] ${doc.Guild} ${doc.Channel} -> ${doc.Words}`)
         })
