20fabdcd17434fd577943efc35cddf24a8ef7199
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-28 12:30:59 +0200

    added poll plugin

---
+++ src/commands/poll/poll.ts
@@ -0,0 +1,68 @@
+import { Message, MessageEmbed } from "discord.js";
+import { Command } from "../../structures/Command";
+
+const emojiAlphabet = [...'🇦🇧🇨🇩🇪🇫🇬🇭🇮🇯🇰🇱🇲🇳🇴🇵🇶🇷🇸🇹🇺🇻🇼🇽🇾🇿'];
+const sep = "+";
+
+export default new Command({
+    name: "poll",
+    description: "Creates a poll",
+    options: [
+        {
+            name: "question",
+            description: "The topic you are interested in.",
+            type: "STRING",
+            required: true
+        },
+        {
+            name: "choice",
+            description: `[Empty 👍👎 or like "one ${sep} two ${sep} three ${sep} four"]`,
+            type: "STRING",
+            required: false
+        }
+    ],
+    run: async ({ interaction, args }) => {
+
+        const question = args.getString("question");
+        const choice = args.getString("choice");
+
+        if (choice && choice.indexOf(sep) == -1) {
+            const embed = new MessageEmbed().setTitle('📊 ' + question);
+            await interaction.followUp({ embeds: [embed] }).then(async (msg: Message) => {
+                await msg.react('👍');
+                await msg.react('👎');
+            });
+        }
+        else {
+            const embed = new MessageEmbed().setTitle('📊 ' + question);
+
+            const options = choice.split(sep).map(e => e.trim());
+            if (options.length > emojiAlphabet.length) {
+                return await interaction.followUp(
+                    {
+                        content: `Please don\'t input more than ${emojiAlphabet.length} options.`,
+                        ephemeral: true
+                    })
+                    .then((reply: Message) => {
+                        setTimeout(() => {
+                            reply.delete();
+                        }, 2000);
+                    });
+            }
+
+            options.forEach((option, i) => {
+                options[i] = `${emojiAlphabet[i]} ${option}`
+            });
+
+            embed.setDescription(options.join('\n\n'));
+
+            await interaction.followUp({ embeds: [embed] })
+                .then(async (msg: Message) => {
+                    options.forEach(async (_, i) => {
+                        await msg.react(emojiAlphabet[i]);
+                    })
+                });
+
+        }
+    }
+})
