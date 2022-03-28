5f648e13b68a12f6cd2112688adcf11c37b671e5
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-28 20:21:54 +0200

    tictactoe cleanup

--- src/events/interactionCreate-tictacoe.ts
+++ src/events/interactionCreate-tictacoe.ts
@@ -1,4 +1,4 @@
-import { GuildManager, GuildMember, Message, MessageActionRow, MessageActionRowComponent, MessageButton, MessageComponentInteraction } from "discord.js";
+import { GuildMember, Message, MessageActionRow, MessageButton, MessageComponentInteraction } from "discord.js";
 import { Event } from "../structures/Events";
 import { errorHandler } from "../utils/errorHandler";
 
@@ -19,27 +19,39 @@ const updateGrid = async function (interaction: MessageComponentInteraction) {
     const member = interaction.member as GuildMember;
     const { displayName } = member;
 
-    message.components.forEach((actionRow: MessageActionRow, i) => {
+    message.components.forEach((actionRow: MessageActionRow, y) => {
         actionRow.components.forEach((button: MessageButton) => {
+            const { customId, label } = button;
             // header
-            if (i == 0) {
-                if (button.customId.includes("tttowner")) {
-                    owner = button.label;
-                } else if (button.customId.includes("tttopponent")) {
-                    opponent = button.label;
+            if (y == 0) {
+                if (customId.includes("tttowner")) {
+                    owner = label;
+                } else if (customId.includes("tttopponent")) {
+                    opponent = label;
                 }
             }
 
-            if (button.label === 'X') xs++;
-            else if (button.label === 'O') os++;
+            if (label === 'X') xs++;
+            else if (label === 'O') os++;
         });
     });
 
     if (![owner, opponent].includes(displayName)) {
-        return await interaction.reply({ content: "You can't play this game!", ephemeral: true })
+        return await interaction.reply(
+            {
+                content: "Respect others game :pray:. You are not invited to this match!",
+                ephemeral: true
+            })
     }
 
     const xs_turn = xs <= os;
+    const y = parseInt(interaction.customId[3]);
+    const x = parseInt(interaction.customId[4]);
+
+    const buttonPressed = message.components[y].components[x] as MessageButton;
+
+    if (buttonPressed.label !== '_')
+        return await interaction.reply({ content: "Someone already moved there!", ephemeral: true });
 
     if (xs_turn && displayName != owner) {
         return await interaction.reply({ content: `Wait for ${owner} turn!`, ephemeral: true })
@@ -48,40 +60,13 @@ const updateGrid = async function (interaction: MessageComponentInteraction) {
         return await interaction.reply({ content: `Wait for ${opponent} turn!`, ephemeral: true })
     }
 
-    const i = parseInt(interaction.customId[3]),
-        j = parseInt(interaction.customId[4]);
-
-    const buttonPressed = message.components[i].components[j] as MessageButton;
-
-    if (buttonPressed.label !== '_')
-        return await interaction.reply({ content: "Someone already played there!", ephemeral: true });
-
     buttonPressed.label = xs_turn ? 'X' : 'O';
     buttonPressed.style = xs_turn ? "SUCCESS" : "DANGER";
 
     const styleToNumber = style => style === "SECONDARY" ? 2 : style === "SUCCESS" ? 3 : 4;
 
-    const components = [];
-    message.components.forEach((actionRow: MessageActionRow, i) => {
-        // header
-        if (i == 0) {
-            components.push(message.components[0]);
-            return;
-        }
-        // board
-        components.push({ type: 1, components: [] });
-        actionRow.components.forEach((button: MessageButton) => {
-            components[components.length - 1].components.push(
-                {
-                    type: 2,
-                    label: button.label,
-                    style: styleToNumber(button.style),
-                    custom_id: button.customId
-                }
-            );
-        });
-    });
+    buttonPressed.setStyle(styleToNumber(buttonPressed.style));
 
-    await message.edit({ components: components });
+    await message.edit({ components: message.components });
     await interaction.deferUpdate();
 }
