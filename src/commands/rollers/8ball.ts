759e717b6d66f34ee53221df9e3ab7a2767e0718
Xian55 <367101+Xian55@users.noreply.github.com>
2022-03-30 13:34:31 +0200

    8ball added color coded emojis

--- src/commands/rollers/8ball.ts
+++ src/commands/rollers/8ball.ts
@@ -2,26 +2,29 @@ import { Command } from "../../structures/Command";
 import "../../utils/array"
 
 const table = [
-	"It is certain",
-	"It is decidedly so",
-	"Without a doubt",
-	"Yes, definitely",
-	"You may rely on it",
-	"As I see it, yes",
-	"Most likely",
-	"Outlook good",
-	"Yes",
-	"Signs point to yes",
-	"Reply hazy try again",
-	"Ask again later",
-	"Better not tell you now",
-	"Cannot predict now",
-	"Concentrate and ask again",
-	"Don't count on it",
-	"My reply is no",
-	"My sources say no",
-	"Outlook not so good",
-	"Very doubtful",
+	":blue_square: It is certain.",
+	":blue_square: It is decidedly so",
+	":blue_square: Without a doubt",
+	":blue_square: Yes, definitely",
+	":blue_square: You may rely on it",
+
+	":green_square: As I see it, yes",
+	":green_square: Most likely",
+	":green_square: Outlook good",
+	":green_square: Yes",
+	":green_square: Signs point to yes",
+
+	":yellow_square: Reply hazy, try again",
+	":yellow_square: Ask again later",
+	":yellow_square: Better not tell you now",
+	":yellow_square: Cannot predict now",
+	":yellow_square: Concentrate and ask again",
+
+	":red_square: Don't count on it",
+	":red_square: My reply is no",
+	":red_square: My sources say no",
+	":red_square: Outlook not so good",
+	":red_square: Very doubtful",
 ];
 
 export default new Command({
