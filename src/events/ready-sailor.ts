82a3d82a7818afa7e6b52fb9940d7b10a63e4836
Xian55 <367101+Xian55@users.noreply.github.com>
2022-07-17 03:56:45 +0200

    Update sailor plugin.

--- src/events/ready-sailor.ts
+++ src/events/ready-sailor.ts
@@ -7,15 +7,17 @@ import { client } from "..";
 import { TextChannel } from "discord.js";
 
 export default [new Event('ready', async () => {
-    if (!fs.existsSync(mondayFile)) return;
     let job = new CronJob('0 4 * * MON', task);
     job.start();
 })];
 
-const file = "it_be_monday.mp4";
-const mondayFile = path.resolve(process.env.user_path, file);
+const n = 4;
 
 async function task() {
+    const file = `it_be_monday_${Math.floor(Math.random() * n)}.mp4`;
+    const mondayFile = path.resolve(process.env.user_path, file);
+    if (!fs.existsSync(mondayFile)) return;
+
     const { channels } = client;
     const channel = await channels.fetch(process.env.plebek_post) as TextChannel; // playground - 789904472863146047
     if (!channel) return;
@@ -31,5 +33,4 @@ async function task() {
             ]
         }
     );
-
 }
